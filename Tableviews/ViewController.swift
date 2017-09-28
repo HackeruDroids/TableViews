dedffbe84993c8538676cafa22e68db78e64e6e1
hackerudroids <hackerudroids@gmail.com>
2017-09-28 17:57:19 +0300

    Initial Commit

---
+++ Tableviews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tableviews
+//
+//  Created by hackeru on 8 Tishri 5778.
+//  Copyright © 5778 hackeru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
